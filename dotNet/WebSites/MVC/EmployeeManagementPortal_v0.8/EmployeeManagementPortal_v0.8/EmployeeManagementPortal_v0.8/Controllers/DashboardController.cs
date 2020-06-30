fc3ab87bd2db680ff35177263af91c9555aad97b
Yahudha James <yahudhajames@outlook.com>
2020-06-30 22:13:55 +0530

    Development of same project with different version

---
+++ dotNet/WebSites/MVC/EmployeeManagementPortal_v0.8/EmployeeManagementPortal_v0.8/EmployeeManagementPortal_v0.8/Controllers/DashboardController.cs
@@ -0,0 +1,98 @@
+﻿using EmployeeManagementPortal_v0._8.Models;
+using EmployeeManagementPortal_v0._8.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementPortal_v0._8.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private EmployeeManagementPortal_v0._8.Models.DAL.IModelRepository db;
+
+        public DashboardController()
+        {
+            this.db = new ModelRepository(new Models.EmployeeManagementAppEntities());
+        }
+
+
+
+
+
+        // GET: Dashboard
+        public ActionResult DashIndex()
+        {
+            var objUser = db.GetUsers();
+            return View(objUser);
+        }
+
+
+        public ActionResult Edit(int Id)
+        {
+            var objUser = db.GetUserbyID(Id);
+            return View(objUser);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User objUser)
+        {
+
+            var user = new User
+            {
+                Id = objUser.Id,
+                Role = objUser.Role,
+                UserName = objUser.UserName,
+                Image = objUser.Image
+
+            };
+
+            using(var db = new EmployeeManagementAppEntities())
+            {
+                db.Users.Attach(user);
+                db.Entry(user).Property(x => x.UserName).IsModified = true;
+                db.Entry(user).Property(x => x.Role).IsModified = true;
+                db.Entry(user).Property(x => x.Image).IsModified = true;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("DashIndex");
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var objUser = db.GetUserbyID(Id);
+            return View(objUser);
+        }
+
+
+        public ActionResult Delete(int Id)
+        {
+            var objUser = db.GetUserbyID(Id);
+            return View(objUser);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(User objUser)
+        {
+            db.DeleteUser(objUser.Id);
+            db.Save();
+            return RedirectToAction("DashIndex");
+
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User objUser)
+        {
+            db.InsertUser(objUser);
+            db.Save();
+            return RedirectToAction("DashIndex");
+        }
+    }
+}
---
+++ dotNet/WebSites/MVC/EmployeeManagementPortal_v0.8/EmployeeManagementPortal_v0.8/EmployeeManagementPortal_v0.8/Models/DAL/ModelRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagementPortal_v0._8.Models.DAL
+{
+    public class ModelRepository : IModelRepository
+    {
+
+        private EmployeeManagementAppEntities db = new EmployeeManagementAppEntities();
+
+        public ModelRepository(EmployeeManagementAppEntities db)
+        {
+            this.db = db;
+        }
+
+        public void DeleteUser(int Id)
+        {
+            User objUser = db.Users.Find(Id);
+            db.Users.Remove(objUser);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Role> GetRoles()
+        {
+            return db.Roles.ToList();
+        }
+
+        public User GetUserbyID(int Id)
+        {
+            User objUser = db.Users.Find(Id);
+            return objUser;
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return db.Users.ToList();
+        }
+
+        public void InsertUser(User objUser)
+        {
+            db.Users.Add(objUser);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
