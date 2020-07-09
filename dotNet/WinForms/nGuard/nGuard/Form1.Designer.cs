c5e300bf8a885e26d499bdfffb72bb58cdc3e96d
Yahudha James <yahudhajames@outlook.com>
2020-07-10 01:56:42 +0530

    nGuard - Prevent your system from auto sleep

---
+++ dotNet/WinForms/nGuard/nGuard/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace nGuard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 48);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 18);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(83, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 18);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(117, 74);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown_1);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ dotNet/WinForms/nGuard/nGuard/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nGuard
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            try
+            {
+                Sleep.IsStarted = false;
+                InitializeComponent();
+                this.MaximizeBox = false;
+                button1.Image = Properties.Resources.Start;
+                button2.Image = Properties.Resources.mini;
+                button3.Image = Properties.Resources.close;
+
+                FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            }
+            catch
+            {
+                MessageBox.Show("We have ran into techincal glitch! App will be automatically closed.");
+            }
+        }
+
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.WindowState == FormWindowState.Minimized)
+                {
+                    Hide();
+                    notifyIcon.Visible = true;
+
+
+                    if (Sleep.IsStarted)
+                    {
+                        notifyIcon.Icon = nGuard.Properties.Resources.start_thick;
+                        notifyIcon.Text = "Prevent Sleep Started";
+                        notifyIcon.ShowBalloonTip(1000, "Minimized to Tray!", "Prevent Sleep is Started", new ToolTipIcon());
+                    }
+                    else
+                    {
+                        notifyIcon.Icon = nGuard.Properties.Resources.stop_thick;
+                        notifyIcon.Text = "Prevent Sleep Stopped";
+                        notifyIcon.ShowBalloonTip(1000, "Minimized to Tray!", "Prevent Sleep is Stopped", new ToolTipIcon());
+                    }
+                    //notifyIcon.ShowBalloonTip(1000);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("We have ran into techincal glitch! We will try to restart the Application.");
+                Application.Restart();
+            }
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                Show();
+                this.WindowState = FormWindowState.Normal;
+                notifyIcon.Visible = false;
+            }
+            catch
+            {
+                MessageBox.Show("We have ran into techincal glitch! We will try to restart the Application.");
+                Application.Restart();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Sleep.IsStarted)
+                {
+                    Sleep.AllowSleep();
+                    button1.Image = Properties.Resources.Start;
+                }
+                else
+                {
+                    button1.Image = Properties.Resources.Stop;
+                    Sleep.PreventSleep();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("We have ran into techincal glitch! We will try to restart the Application.");
+                Application.Restart();
+            }
+        }
+
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+
+        private void Form1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    ReleaseCapture();
+                    SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("We have ran into techincal glitch! We will try to restart the Application.");
+                Application.Restart();
+            }
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+            catch
+            {
+                MessageBox.Show("We have ran into techincal glitch! We will try to restart the Application.");
+                Application.Restart();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                this.Close();
+                Environment.Exit(0);
+            }
+            catch
+            {
+                MessageBox.Show("We have ran into techincal glitch! We will try to close the Application.");
+                Environment.Exit(0);
+
+            }
+        }
+    }
+
+
+    internal static class Sleep
+    {
+
+        public static void PreventSleep()
+        {
+            IsStarted = true;
+            SetThreadExecutionState(ExecutionState.EsContinuous | ExecutionState.EsSystemRequired);
+        }
+
+        public static void AllowSleep()
+        {
+            IsStarted = false;
+            SetThreadExecutionState(ExecutionState.EsContinuous);
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern ExecutionState SetThreadExecutionState(ExecutionState esFlags);
+
+        [Flags]
+        private enum ExecutionState : uint
+        {
+            EsAwaymodeRequired = 0x00000040,
+            EsContinuous = 0x80000000,
+            EsDisplayRequired = 0x00000002,
+            EsSystemRequired = 0x00000001
+        }
+        public static bool IsStarted { get; set; }
+
+
+    }
+
+
+}
